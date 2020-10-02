7e51f988244cf7c77ae27597d783f71dc3d8c696
lhxone <55795964+lhxone@users.noreply.github.com>
2020-10-02 13:20:09 +0800

    10.2.2

--- 10.2作业.cpp
+++ 10.2作业.cpp
@@ -2,28 +2,95 @@
 
 using namespace std;
 
-typedef struct node{
+typedef struct node
+{
     int data;
-    node *next;
-}*LinkList;
+    struct node *next;
+} *NodeList;
 
-typedef struct SqList {
+typedef struct SqList {     //顺序表
     int *elem;
     int len;
     int size;
-}*SList;
+};
 
-void CreatList(LinkList &L,int n){
-    LinkList p;
-    L = new (node);
-    L->next = NULL;
-    for (int i = 0; i < n; ++i) {
-        p = (LinkList) malloc(sizeof(node));
-        p->data = abs(n/2-i);
-        p->next = L->next;
-        L->next = p;
+
+NodeList *createEnd(NodeList &head,int len)         // 尾插法
+{
+    head = (node *)malloc(sizeof(NodeList));
+    head->next = NULL;
+    NodeList end = head;
+    for (int i = 0; i < len; i++) {
+        NodeList p = (NodeList)malloc(sizeof(node));
+        p->data= rand()%100;
+        end->next = p;
+        end = p;
+    }
+    end->next = NULL;
+}
+
+void Print(NodeList head)
+{
+    int i = 0;
+    if (head == NULL)
+        return;
+    NodeList p = head->next;
+    while (p != NULL) {
+        printf("%d  ", p->data);
+        p = p->next;
+        if ((i+1)%5 == 0){
+            cout<<endl;
+        }
+        i++;
+    }
+    cout<<endl;
+}
+
+void DeletElem(NodeList &head,SqList &T)
+{
+    int i = 0;
+    NodeList p,q,r,temp;
+    NodeList s = head;
+    T.size = 1000;
+    T.elem = (int*)malloc(T.size * sizeof(int));
+    T.len = 0;
+    p = s->next;
+    int flag = 0;
+    while(p != NULL)
+    {
+        q = p;
+        while(q->next != NULL)
+        {
+            if(q->next->data == p->data)
+            {
+                flag = 1;
+                r = q->next;
+                q->next = r->next;
+                free(r);
+                T.elem[T.len++] = p->data+i*100;
+            }
+            else
+            {
+                q = q->next;
+            }
+        }
+        if (flag == 0)
+        {
+            s = p;
+            p = p->next;
+        }
+        else
+        {
+            flag = 0;
+            temp = p;
+            p = p->next;
+            s->next = p;
+            free(temp);
+        }
+        i++;
     }
 }
+
 
 void CreatList(SqList &L,int n){
     L.elem  = (int*)malloc(n*sizeof(int));
@@ -31,16 +98,6 @@ void CreatList(SqList &L,int n){
         L.elem[i] = rand()%100;
     }
     L.len = n;
-}
-
-void PrintList(LinkList L){
-    int sum=0;
-    for (LinkList p=L->next;p != NULL;p = p->next){
-        cout<<p->data<<" ";
-        sum++;
-    }
-    cout<<endl;
-//    cout<<endl<<sum<<endl;
 }
 
 void PrintList(SqList L){
@@ -51,33 +108,6 @@ void PrintList(SqList L){
         }
     }
     cout<<endl;
-}
-
-
-void Delet(LinkList &L,int i){
-    int cnt=0;
-    LinkList p;
-    p = L;
-    while (p&&(cnt < i-1)){
-        cnt++;
-        p = p->next;
-    }
-    p->next = (*(p->next)).next;
-}
-
-void DeletElem(LinkList &L){
-    int i=0,j=0;
-    LinkList head = L;
-    LinkList p,q;
-    for(p = head;p->next != NULL;p = p->next,i++){
-        for(q = p;q->next != NULL;q = q->next,j++){
-            if (p->data == q->next->data){
-                Delet(L,i+j);
-            }
-        }
-        j=0;
-    }
-    return;
 }
 
 void DeletElem(SqList &L,SqList &T){
@@ -119,5 +149,20 @@ int main()
         cout<<"无重复元素"<<endl;
     }
     //TODO:删除链表中的重复元素
-    
+    NodeList head;
+    createEnd(head,n);
+    cout<<"单链表创建完成:"<<endl;
+    Print(head);
+    DeletElem(head,T);
+    cout<<"单链表删除完成:"<<endl;
+    Print(head);
+    if (T.len != 0){
+        cout<<"删除的元素为:"<<endl;
+        for (int i = 0; i < T.len; ++i) {
+            cout<<"原位置为:"<<T.elem[i]/100+1<<"->"<<T.elem[i]%100<<" ";
+            cout<<endl;
+        }
+    } else{
+        cout<<"无重复元素"<<endl;
+    }
 }
