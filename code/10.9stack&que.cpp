b029fa3a7923c64b7e4968e8f6940fc666a0f767
lhxone <55795964+lhxone@users.noreply.github.com>
2020-10-09 20:49:59 +0800

    Update 10.9stack&que.cpp

--- code/10.9stack&que.cpp
+++ code/10.9stack&que.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-//#include <stack>
+#include <stack>
 //#include <queue>
 #include <iomanip>
 
@@ -40,8 +40,11 @@ void push(Slink &s,int n){
 void pop(Slink &s){
     Snode *p;
     p = s.base;
-    if (s.base == s.top)
+    if ((s.base == s.top)&&(s.base != NULL)){
+        free(s.base);
+        s.base = s.top = NULL;
         return;
+    }
     while (p->next != s.top) p = p->next;
     s.top = p;
     free(p->next);
@@ -50,6 +53,13 @@ void pop(Slink &s){
 
 int top(Slink s){
     return s.top->data;
+}
+
+int empty(Slink s){
+    if (s.base == NULL)
+        return 1;
+    else
+        return 0;
 }
 
 void InitQue(Qlink &q){
@@ -94,10 +104,13 @@ int main() {
     for (int i = 0; i < temp; ++i) {
         InsertQue(q,rand()%100);
     }
+    cout<<"输出队列："<<endl;
     PrintQue(q);
     DelQue(q,temp);
     cout<<endl;
+    cout<<"出队元素：";
     cout<<temp<<endl;
+    cout<<"出队后队列："<<endl;
     PrintQue(q);
     cout<<endl;
     //TODO:
@@ -107,20 +120,54 @@ int main() {
     for (int i = 0; i < temp; ++i) {
         push(s,rand()%100);
     }
+    cout<<"压栈完成："<<endl;
     for (int i = 0; i < temp; ++i) {
         int t = top(s);
         cout<<t<<" ";
         pop(s);
     }
     cout<<endl;
+    int n,r,d;
+    cout<<"进制转换,输入十进制数n与目标进制r:"<<endl;
+    InitStack(s);
+    cin>>n>>r;
+    while (n!=0){
+        d = n%r;
+        n = n/r;
+        push(s,d);
+    }
+    cout<<"转换完成：";
+    while (!empty(s)){
+        std::cout<<top(s);
+        pop(s);
+    }
+    cout<<"("<<r<<")"<<endl;
+    cout<<"括号匹配，输入一行括号："<<endl;
+    char a[100];
+    stack<char>(S);
+    cin>>a;
+    for (int i = 0; a[i] != '\0'; ++i) {
+        if (S.empty()){
+            S.push(a[i]);
+        } else if (((S.top() == '(')&&(a[i] == ')'))||((S.top() == '[')&&(a[i] == ']'))||((S.top() == '{')&&(a[i] == '}'))){
+            S.pop();
+        } else if (a[i] == '('||a[i] == '['||a[i] == '{'){
+            S.push(a[i]);
+        }
+    }
+    if (S.empty()){
+        cout<<"YES";
+    } else{
+        cout<<"NO";
+    }
 }
 
 
 /*
  * //TODO:进制转换
  *     int n,d,r;
-    std::stack<int>(s);
-    std::cin>>n>>r;
+    stack<int>(s);
+    cin>>n>>r;
     while (n!=0){
         d = n%r;
         n = n/r;
