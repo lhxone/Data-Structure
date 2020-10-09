6628c143963d54707c296753fa10a50fad052af5
lhxone <55795964+lhxone@users.noreply.github.com>
2020-10-09 17:45:33 +0800

    Create 10.9随便写的.cpp

---
+++ code/10.9随便写的.cpp
@@ -0,0 +1,83 @@
+#include <iostream>
+#include <stack>
+#include <queue>
+#pragma GCC optimize(2)
+using namespace std;
+
+typedef struct node {
+    int data;
+    node *next;
+}Qnode;
+
+typedef struct{
+    Qnode *rear,*front;
+}Qlink;
+
+void InitQue(Qlink &q){
+    Qnode *p = new (node);
+    q.rear = q.front = p;
+}
+
+void InsertQue(Qlink &q,int n){
+    Qnode *p = new (node);
+    p->data = n;
+    p->next = NULL;
+    q.rear->next = p;
+    q.rear = p;
+}
+
+void PrintQue(Qlink q){
+    Qnode *p;
+    p = q.front->next;
+    while (p != NULL){
+        cout<<p->data<<" ";
+        p = p->next;
+    }
+}
+
+int main() {
+    int temp;
+    Qlink q;
+    InitQue(q);
+    for (int i = 0; i < 3; ++i) {
+        cin>>temp;
+        InsertQue(q,temp);
+    }
+    PrintQue(q);
+}
+/*
+ * //TODO:进制转换
+ *     int n,d,r;
+    std::stack<int>(s);
+    std::cin>>n>>r;
+    while (n!=0){
+        d = n%r;
+        n = n/r;
+        s.push(d);
+    }
+    while (!s.empty()){
+        std::cout<<s.top();
+        s.pop();
+    }
+*/
+
+/*
+ * //TODO:括号匹配
+ * char a[20];
+    stack<char>(s);
+    cin>>a;
+    for (int i = 0; a[i] != '\0'; ++i) {
+        if (s.empty()){
+            s.push(a[i]);
+        } else if (((s.top() == '(')&&(a[i] == ')'))||((s.top() == '[')&&(a[i] == ']'))||((s.top() == '{')&&(a[i] == '}'))){
+            s.pop();
+        } else if (a[i] == '('||a[i] == '['||a[i] == '{'){
+            s.push(a[i]);
+        }
+    }
+    if (s.empty()){
+        cout<<"YES";
+    } else{
+        cout<<"NO";
+    }
+*/
